8bb583930303a07c4181661f22ed8779a0de69d4
littlen101 <nlewis2016@my.fit.edu>
2019-11-24 23:48:43 -0500

    reorganize the projext

---
+++ Memes/loadPhotos.asm
@@ -0,0 +1,16 @@
+.386
+.model flat, STDCALL
+INCLUDE valid.inc
+
+.data
+picOne BYTE "THIS IS WHERE I WOULD PUT THE PICTURE, IF I HAD ONE", 0
+;PUT THE REST OF THE PICTURES HERE
+
+
+
+topIn BYTE 31 DUP (?)
+botIn BYTE 31 DUP (?)
+
+
+
+END
